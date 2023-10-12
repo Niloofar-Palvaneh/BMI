fe0ce10e223aaadf82776fb684949f420d2fd786
niloofar-palvanehcls <niloofarpalvaneh@gmail.com>
2023-10-13 01:32:48 +0330

    init and create ui

---
+++ src/components/title/Title.js
@@ -0,0 +1,10 @@
+
+export default function Title({title}) {
+    return (
+        <>
+            <div className="font-bold text-xl text-gray-600">
+            {title} :
+            </div>
+        </>
+    )
+}
